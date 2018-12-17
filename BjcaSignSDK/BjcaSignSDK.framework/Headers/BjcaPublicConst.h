0541e2e83bd6377ae19ceb5c54e5b1d0095280d7
wuxing <wuxing@bjca.org.cn>
2018-12-17 20:26:01 +0800

    提交

---
+++ BjcaSignSDK/BjcaSignSDK.framework/Headers/BjcaPublicConst.h
@@ -0,0 +1,53 @@
+//
+//  BjcaPublicConst.h
+//  BjcaSignSDK
+//
+//  Created by 吴兴 on 2018/11/20.
+//  Copyright © 2018 szyx. All rights reserved.
+//
+
+#ifndef BjcaPublicConst_h
+#define BjcaPublicConst_h
+
+//SDK地址
+typedef NS_ENUM(NSInteger,BjcaServerType) {
+    //    正式环境
+    BjcaPublic,
+    //    集成环境
+    BjcaIntegrate,
+    //    开发环境
+    BjcaDev,
+    //    测试环境
+    BjcaTest,
+};
+
+//用户希望执行的业务
+typedef NS_ENUM(NSInteger,BjcaBusinessType) {
+//    证书下载
+    BjcaBusinessCertDown,
+//    证书更新
+    BjcaBusinessCertUpdate,
+//    证书密码重置
+    BjcaBusinessCertReset,
+//    修改签章
+    BjcaBusinessStamp,
+//    批量签名
+    BjcaBusinessSignList,
+//    二维码业务
+    BjcaBusinessQrCode,
+//    用户信息
+    BjcaBusinessUserInfo,
+//    免密开启
+    BjcaBusinessFreePin,
+//    查看证书详情
+    BjcaBusinessCertDetail,
+};
+
+//// 免密时间
+//typedef NS_ENUM(NSInteger,BjcaFreePinTime) {
+////    7天
+//    BjcaSevenDay,
+////    30天
+//    BjcaThirtyDay,
+//};
+#endif /* BjcaPublicConst_h */
---
+++ BjcaSignSDK/BjcaSignSDK.framework/Headers/BjcaSignManager.h
@@ -0,0 +1,187 @@
+//
+//  BjcaSignManager.h
+//  BjcaSignSDK
+//
+//  Created by 吴兴 on 2018/11/19.
+//  Copyright © 2018 szyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "BjcaPublicConst.h"
+
+NS_ASSUME_NONNULL_BEGIN
+@protocol BjcaSignDelegate <NSObject>
+//相关代理协议
+-(void)BjcaFinished:(NSDictionary*)backParam;
+
+@end
+
+@interface BjcaSignManager : NSObject
+
+#pragma mark - 初始化相关
+//回调对象
+@property (nonatomic,weak) id<BjcaSignDelegate> bjcaSignDelegate;
+
+
+/**
+ 初始化
+
+ @return BjcaSignManager的实例化对象
+ */
++(BjcaSignManager *)bjcaShareBjcaSignManager;
+
+/**
+ 设置环境
+
+ @param serverType 环境枚举
+ */
++(void)bjcaSetServerURL:(BjcaServerType)serverType;
+
+#pragma mark - 证书相关
+/**
+ 证书下载
+
+ @param clientId 厂商的clientId
+ @param phoneNum 下载证书的手机号
+ @param viewCtrl 当前页的ViewCtrl
+ */
+-(void)bjcaCertDown:(NSString*)clientId phoneNum:(NSString*)phoneNum curViewCtrl:(UIViewController*)viewCtrl;
+
+/**
+ 证书更新
+ 
+ @param clientId 厂商的clientId
+ @param viewCtrl 当前页的ViewCtrl
+ */
+-(void)bjcaCertUpdate:(NSString*)clientId curViewCtrl:(UIViewController*)viewCtrl;
+
+/**
+ 证书密码重置
+ 
+ @param clientId 厂商的clientId
+ @param viewCtrl 当前页的ViewCtrl
+ */
+-(void)bjcaCertReset:(NSString*)clientId curViewCtrl:(UIViewController*)viewCtrl;
+
+/**
+ 签章配置
+
+ @param clientId 厂商的clientId
+ @param viewCtrl 当前页的ViewCtrl
+ @param navColor 导航栏颜色，可为nil
+ @param fontColor 导航栏颜色，可为nil
+ */
+-(void)bjcaSetStamp:(NSString*)clientId curViewCtrl:(UIViewController*)viewCtrl navColor:(UIColor * _Nullable)navColor navFontColor:(UIColor * _Nullable)fontColor;
+
+#pragma mark - 签名相关
+
+
+/**
+ 批量签名
+
+ @param uniqueIds 批量处方的uniqueId数组
+ @param clientId 第三方对应的clientId
+ @param viewCtrl 当前页的ViewCtrl
+ */
+- (void)bjcaBatchSignList:(NSMutableArray *)uniqueIds userClientId:(NSString *)clientId curViewCtrl:(UIViewController*)viewCtrl;
+
+
+/**
+ 二维码扫码字符串签名
+
+ @param qrString 二维码扫码解析得到的字符串
+ @param clientId 第三方对应的clientId
+ @param viewCtrl 当前页的ViewCtrl
+ */
+- (void)bjcaQrSign:(NSString *)qrString userClientId:(NSString *)clientId curViewCtrl:(UIViewController*)viewCtrl;
+
+
+/**
+ 开启免密签名
+
+ @param time 免密时间
+ */
+- (void)bjcaFreePinSign:(int)time clientId:(NSString *)clientId curViewCtrl:(UIViewController*)viewCtrl;
+
+#pragma mark 用户信息相关
+/**
+ 获取用户信息
+
+ @param clientId 第三方对应的clientId
+ */
+- (void)bjcaUserInfo:(NSString *)clientId;
+
+
+/**
+ 打开证书详情页
+
+ @param clientId 第三方对应的clientId
+ @param viewCtrl 当前活跃的ctrl
+ */
+- (void)bjcaCertDetail:(NSString *)clientId curViewCtrl:(UIViewController*)viewCtrl navColor:(UIColor * _Nullable)navColor navFontColor:(UIColor * _Nullable)fontColor;
+#pragma mark - 工具类
+
+/**
+ 获取当前类地址
+
+ @return 地址
+ */
++ (NSString*)bjcaAddress;
+
+
+/**
+ 是否存在证书
+
+ @return BOOL型，YES存在，NO不存在
+ */
++ (BOOL)bjcaExistsCert;
+
+
+/**
+ 是否存在签章
+
+ @return BOOL型，YES存在，NO不存在
+ */
++ (BOOL)bjcaExistsStamp;
+
+
+/**
+ 获取openId
+
+ @return openId
+ @note 若证书不存在则返回nil
+ */
++ (NSString *)bjcaOpenId;
+
+
+/**
+ 获取当前版本号
+
+ @return version
+ */
++ (NSString *)bjcaVersion;
+
+
+/**
+ 移除本地证书
+ */
++ (void)bjcaRemoveCert;
+
+
+/**
+ 当前是否处于免密状态
+
+ @return BOOL YES，免密/NO，非免密
+ @note OAuth登录必须输入密码，无法免密
+ */
++ (BOOL)bjcaExistsFreePin;
+
+
+/**
+ 清除免密状态
+ */
++ (void)bjcaRemovePin;
+@end
+
+NS_ASSUME_NONNULL_END
